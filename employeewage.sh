68ca6db290f882faa33094d983d2a0470de75213
Devshree Moghe <devshree0602@gmail.com>
2024-01-13 12:54:08 +0530

    switch case implemented

--- employeewage.sh
+++ employeewage.sh
@@ -1,6 +1,19 @@
 wageHour=20
 partTimeHour=4
-partTimeWage=$((wageHour*partTimeHour))
+fullTimeHour=8
+echo "Enter 1 for fulltime wage and 2 for part time wage"
+read num
 
-echo "Part Time Wage of Employee is"
-echo $partTimeWage
+case $num in
+ 1)
+    fullTimeWage=$((wageHour*fullTimeHour))
+    echo "full time wage of employee is"
+    echo $fullTimeWage
+    ;;
+ 2)
+    partTimeWage=$((wageHour*partTimeHour))
+    echo "Part time wage of employee is"
+    echo $partTimeWage
+    ;;
+
+esac
