0a63712efed43a5b11e567e62fc80a70750a676c
Devshree Moghe <devshree0602@gmail.com>
2024-01-13 13:17:56 +0530

    Monthly wage calculated

--- employeewage.sh
+++ employeewage.sh
@@ -1,19 +1,11 @@
 wageHour=20
-partTimeHour=4
 fullTimeHour=8
-echo "Enter 1 for fulltime wage and 2 for part time wage"
-read num
+partTimeHour=4
+month=20
+nohours=$((month*fullTimeHour))
+monthlyWage=$((nohours*wageHour))
 
-case $num in
- 1)
-    fullTimeWage=$((wageHour*fullTimeHour))
-    echo "full time wage of employee is"
-    echo $fullTimeWage
-    ;;
- 2)
-    partTimeWage=$((wageHour*partTimeHour))
-    echo "Part time wage of employee is"
-    echo $partTimeWage
-    ;;
+echo "monthly wage is"
 
-esac
+echo $monthlyWage
+
