1b6de5b9a7b885f0504e9c3016070ede4ea1c0d3
Devshree Moghe <devshree0602@gmail.com>
2024-01-16 20:55:03 +0530

    Conditional wage calculated

--- employeewage.sh
+++ employeewage.sh
@@ -1,11 +1,22 @@
-wageHour=20
-fullTimeHour=8
-partTimeHour=4
-month=20
-nohours=$((month*fullTimeHour))
-monthlyWage=$((nohours*wageHour))
+total_days=20
+total_hours=100
+hourWage=8
 
-echo "monthly wage is"
+curr_hr=0
+curr_day=1
 
-echo $monthlyWage
+final_wage=0
+
+while [[ $curr_day -le $total_days  && $curr_hr -le $total_hours ]]
+do
+   dailywage=$(($hour_wage+8))
+   final_wage=$(($dailywage+$final_wage))
+   curr_day=$(($curr_day+1))
+   curr_hr=$(($curr_hr+1))
+done
+
+echo "Total wages are:"
+
+echo $final_wage
+
 
